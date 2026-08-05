d9d83ccb11127342b3bc3cdee698b4a552a6998c
mohitmahla <mohitmahla001@.com>
2026-08-05 10:39:48 +0530

    Simplified service worker - no caching, always fetch fresh

--- sw.js
+++ sw.js
@@ -1,28 +1,18 @@
-const CACHE_NAME = "patez-cache-v3";
+const CACHE_NAME = "patez-cache-v4";
 
-const CORE_ASSETS = [
-    "./",
-    "./index.html",
-    "./style.css",
-    "./script.js"
-];
+self.addEventListener("install", (event) => {
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) =>
+            Promise.all(keys.map((key) => caches.delete(key)))
+        )
+    );
+    self.clients.claim();
+});
 
 self.addEventListener("fetch", (event) => {
-
-    if (event.request.method !== "GET" || event.request.url.includes("/api/")) {
-        return;
-    }
-
-    event.respondWith(
-        fetch(event.request)
-            .then((response) => {
-                const responseClone = response.clone();
-                caches.open(CACHE_NAME).then((cache) => {
-                    cache.put(event.request, responseClone);
-                });
-                return response;
-            })
-            .catch(() => caches.match(event.request))
-    );
-
+    event.respondWith(fetch(event.request));
 });
