4c3dc0f69848bc7766619347bc1d4a7554d7f224
mohitmahla <mohitmahla001@.com>
2026-08-06 22:15:13 +0530

    Disabled caching for instant customer updates

--- sw.js
+++ sw.js
@@ -1,18 +1,25 @@
-const CACHE_NAME = "patez-cache-v4";
+const CACHE_NAME = 'shiv-patez-v2';
 
-self.addEventListener("install", (event) => {
-    self.skipWaiting();
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
 });
 
-self.addEventListener("activate", (event) => {
-    event.waitUntil(
-        caches.keys().then((keys) =>
-            Promise.all(keys.map((key) => caches.delete(key)))
-        )
-    );
-    self.clients.claim();
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache); // Purana cache delete kar dega
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
 });
 
-self.addEventListener("fetch", (event) => {
-    event.respondWith(fetch(event.request));
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
 });
